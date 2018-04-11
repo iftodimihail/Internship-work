6633c5ece824717be072482ac6d0bca4f793f859
iftodi mihail <33319632+iftodimihail@users.noreply.github.com>
2018-04-11 13:59:10 +0300

    Day one: Ruby tutorial

--- Ruby/blog/app/controllers/articles_controller.rb
+++ Ruby/blog/app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
+
 class ArticlesController < ApplicationController
+  http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
   def index
     @articles = Article.all
   end
@@ -13,12 +15,6 @@ class ArticlesController < ApplicationController
 
   def edit
     @article = Article.find(params[:id])
-
-    if @article.update(article_params)
-      redirect_to @article
-    else
-      render 'edit'
-    end
   end
 
   def create
@@ -29,6 +25,23 @@ class ArticlesController < ApplicationController
     else
       render 'new'
     end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to articles_path
   end
 
   private
